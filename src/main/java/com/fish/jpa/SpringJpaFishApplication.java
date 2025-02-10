96ef65a4783a562922551ce67e733a67c8ec6dee
rlawjdgkr <irlawjdgkr@naver.com>
2025-02-10 15:38:07 +0900

    initial

---
+++ src/main/java/com/fish/jpa/SpringJpaFishApplication.java
@@ -0,0 +1,13 @@
+package com.fish.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaFishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaFishApplication.class, args);
+	}
+
+}
